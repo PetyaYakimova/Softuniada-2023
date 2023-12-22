5f910fa88dd3cb189b126e0a3cec8a99585b5e2d
PetyaYakimova <100902455+PetyaIvovaYakimova@users.noreply.github.com>
2023-12-22 17:06:44 +0200

    Added gitignore, and solution of the first task

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Softuniada-2023
 
-My solutions for the tasks from Softuniada-2023. 
+My solutions for the tasks from Softuniada 2023. 
---
+++ sotfuniada-2023/RowInPascalTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RowInPascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input >= 0)
+            {
+                //The wanted row is the next row after the input number
+                int wantedRow = input + 1;
+
+                Console.WriteLine(string.Join(" ", GetAllNumbersOnRow(wantedRow)));
+            }
+            else
+            {
+                Console.WriteLine("Input was not a valid row number.");
+            }
+        }
+
+        public static List<int> GetAllNumbersOnRow(int rowNumber) 
+        {
+            List<int> rowNumbers = new List<int>();
+            // Every row starts with 1
+            rowNumbers.Add(1);
+
+            for (int i = 1; i < rowNumber; i++)
+            {
+                int nextNumber = rowNumbers.Last() * (rowNumber - i) / i;
+                rowNumbers.Add(nextNumber);
+            }
+
+            return rowNumbers;
+        }
+    }
+}
