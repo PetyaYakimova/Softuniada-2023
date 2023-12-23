acce524f6e105e956f75ebf70bae36fb320633d1
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-23 12:44:03 +0200

    Solution for the sixt task - Biggest Formed Number

---
+++ sotfuniada-2023/BiggestFormedNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace BiggestFormedNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputNumbers = Console.ReadLine().Split(' ').ToList();
+
+            inputNumbers = inputNumbers.OrderByDescending(n => n).ToList();
+            for (int i = 0; i < inputNumbers.Count - 1; i++)
+            {
+                if (inputNumbers[i].Length > inputNumbers[i + 1].Length)
+                {
+                    if (inputNumbers[i].Contains(inputNumbers[i+1]) && (inputNumbers[i + 1][inputNumbers[i + 1].Length - 1] > inputNumbers[i][inputNumbers[i + 1].Length]))
+                    {
+                        string temp = inputNumbers[i];
+                        inputNumbers[i] = inputNumbers[i + 1];
+                        inputNumbers[i + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("", inputNumbers));
+        }
+    }
+}
