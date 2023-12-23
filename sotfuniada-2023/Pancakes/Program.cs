7efb6f037cc767bfbd527c2ce8171a4a51992883
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-23 11:54:02 +0200

    Solution for the fifth task - Pancake

---
+++ sotfuniada-2023/Pancakes/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Pancakes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputPancakeParts = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int maxLengthSum = int.MinValue;
+            int maxLengthStartIndex = -1;
+            int maxLengthNumberOfElements = 0;
+
+            for (int i = 0; i < inputPancakeParts.Length; i++)
+            {
+                for (int j = i; j < inputPancakeParts.Length; j++)
+                {
+                    int currentLengthSum = 0;
+                    for (int el = i; el <= j; el++)
+                    {
+                        currentLengthSum += inputPancakeParts[el];
+                    }
+
+                    if (currentLengthSum > maxLengthSum)
+                    {
+                        maxLengthNumberOfElements = j - i + 1;
+                        maxLengthStartIndex = i;
+                        maxLengthSum = currentLengthSum;
+                    }
+                    else if (currentLengthSum == maxLengthSum && maxLengthNumberOfElements < j - i + 1)
+                    {
+                        maxLengthNumberOfElements = j - i + 1;
+                        maxLengthStartIndex = i;
+                    }
+                }
+            }
+
+            Console.WriteLine(maxLengthSum + " " + maxLengthStartIndex + " " + (maxLengthNumberOfElements + maxLengthStartIndex - 1));
+        }
+    }
+}
