7b254db494877f70d86385850fa91d05ec4bf1eb
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-23 11:31:36 +0200

    Solution for the fourth task - Rotate Matrix

---
+++ sotfuniada-2023/RotateMatrix/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RotateMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] finalMatrix = new int[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                int[] inputMatrixRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    finalMatrix[j, matrixSize-1-i] = inputMatrixRow[j];
+                }
+            }
+
+
+            //Prinnt the result matrix
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    Console.Write(finalMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
