d8c266433da1ef70ebb50b347b1536d6190d821b
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-23 11:20:28 +0200

    Solution for the third task - Longest Valid Parantheses

---
+++ sotfuniada-2023/LongestValidParentheses/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LongestValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputCharacters = Console.ReadLine().ToCharArray();
+            int longestValidLength = 0;
+
+            //Get the longest valid length of elements starting from each of the elements
+            for (int i = 0; i < inputCharacters.Length; i++)
+            {
+                int currentValidLength = 0;
+
+                bool lastCharIsOpeningBracket = false;
+                for (int j = i; j < inputCharacters.Length; j++)
+                {
+                    if ((inputCharacters[j] == '(' && !lastCharIsOpeningBracket) || (inputCharacters[j] == ')' && lastCharIsOpeningBracket))
+                    {
+                        currentValidLength++;
+                        lastCharIsOpeningBracket = !lastCharIsOpeningBracket;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (lastCharIsOpeningBracket)
+                {
+                    currentValidLength--;
+                }
+
+                if (currentValidLength > longestValidLength)
+                {
+                    longestValidLength = currentValidLength;
+                }
+            }
+
+            Console.WriteLine(longestValidLength);
+        }
+    }
+}
--- sotfuniada-2023/Rocket/Program.cs
+++ sotfuniada-2023/Rocket/Program.cs
@@ -140,7 +140,6 @@
                 }
 
                 Console.WriteLine(string.Join("", row));
-
             }
         }
     }
